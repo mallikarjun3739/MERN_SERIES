6bc8f92f12b99bc275507da67f205636e61e9302
mallikarjun3739 <mallikarjun16455@gmail.com>
2023-03-29 10:57:08 +0530

    pending project

--- social/src/components/Share/Share.jsx
+++ social/src/components/Share/Share.jsx
@@ -1,19 +1,19 @@
 import "./share.css";
-import PermMedia from "@mui/icons-material/PermMedia";
+import PermMediaIcon from "@mui/icons-material/PermMedia";
 
 export default function Share() {
   return (
     <div className="share">
       <div className="shareWrapper">
         <div className="shareTop">
-          <img src="/assets/person/1.jpeg" alt="" className="shareProfileImg"/>
-          <input placeholder="What's in your mind ?" className="shareInput" />
+          <img src="/assets/person/1.jpeg" alt="" className="shareProfileImg" />
+          <input placeholder="Hi there ?" className="shareInput" />
         </div>
         <hr className="shareHr" />
         <div className="shareBottom">
           <div className="shareOptions">
             <div className="shareOption">
-              <PermMedia classname="shareIcon" />
+              <PermMediaIcon className="shareIcon" />
               <span className="shareOptionText">Photo or Video</span>
             </div>
           </div>
